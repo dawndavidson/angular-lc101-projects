6d153d29d84c203e1cf528b248d37086946d0755
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-06-17 20:36:22 -0500

    homework

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Very beautiful photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.fineartamerica.com/images-medium-large-5/african-queen-maurice-de-vries.jpg';
+  image3 = 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/a061d024691423.563387d812c3a.jpg';
 
   constructor() { }
 
